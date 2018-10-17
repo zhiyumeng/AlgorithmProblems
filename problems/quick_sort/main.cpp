96f9b8df6b65a98b92aaa739c4badd7a5f6685be
fyyc <zhiyumengxiang@outlook.com>
2018-10-17 14:32:02 +0800

    算法导论上快排的一种实现方式

--- problems/quick_sort/main.cpp
+++ problems/quick_sort/main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+using namespace std;
+
+void exchange(int &a,int &b){
+    if(&a==&b)
+        return;
+    a = a+b;
+    b = a-b;
+    a = a-b;
+}
+
+int partition(int *nums, int p, int r){
+
+    int pivot = nums[r];
+    int left_index = p-1;
+    for(int i=p;i<r;i++)
+        if(nums[i] < pivot) {
+            exchange(nums[i],nums[++left_index]);
+        }
+    exchange(nums[++left_index],nums[r]);
+    return left_index;
+}
+
+void quick_sort(int *nums,int p,int r){//算法导论第三版第95页伪代码版本
+    if(p < r){
+        int pivot_index = partition(nums,p,r);
+        quick_sort(nums,p,pivot_index-1);
+        quick_sort(nums,pivot_index+1,r);
+    }
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int nums[] = {1,3,2,4,5,6,7};
+    for(int i=0;i<7;i++)
+        cout<<nums[i]<<" ";
+    cout<<endl;
+    quick_sort(nums,0,6);
+    for(int i=0;i<7;i++)
+        cout<<nums[i]<<" ";
     return 0;
 }
