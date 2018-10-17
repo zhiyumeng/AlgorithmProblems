99dbb9ad782629c9bf25ff8eb452a08a2601ccf5
fyyc <zhiyumengxiang@outlook.com>
2018-10-17 12:39:13 +0800

    achieve quick sort algorithm
    achieve quick sort algorithm

---
+++ problems/quick_sort/cmake-build-debug/CMakeFiles/quick_sort.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quick_sort.dir/main.cpp.o"
+  "quick_sort.pdb"
+  "quick_sort.exe"
+  "libquick_sort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quick_sort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
